77ca8a4ca899c364f2d6076f9a2d9883242dbfdb
Fabian Irsara <info@fabianirsara.com>
2016-02-22 16:54:09 +0100

    adjust item

--- src/containers/List/Item.js
+++ src/containers/List/Item.js
@@ -8,6 +8,11 @@ import grid from '../../assets/css/grid.css'
 import imagepath, { fetch } from '../../utils/imagepath'
 
 class Item extends Component {
+  handleMouseOver() {
+    let height = this.refs.node.offsetHeight
+    this.refs.image.style.height = height + 'px'
+  }
+
   render() {
     const { page } = this.props
     const path = page.preview
@@ -24,7 +29,7 @@ class Item extends Component {
     )
 
     return (
-      <article className={classes}>
+      <article ref="node" className={classes} onMouseOver={::this.handleMouseOver}>
         <header className={style.itemText}>
           <div className={classnames(grid.container, grid.shortContainer)}>
             <h2 className={style.itemSubTitle}>{page.category}</h2>
@@ -32,7 +37,9 @@ class Item extends Component {
           </div>
         </header>
         <div className={style.preview}>
-          <div className={style.previewImage} style={imageCss} />
+          <div className={style.previewWrap}>
+            <div ref="image" className={style.previewImage} style={imageCss} />
+          </div>
         </div>
       </article>
     )
