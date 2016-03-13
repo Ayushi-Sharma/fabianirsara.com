623e9c686efbb9e60ba4896b87d3133e31540f0a
Fabian Irsara <info@fabianirsara.com>
2016-03-13 12:36:43 +0100

    fix indentation

--- src/utils/screenSize.js
+++ src/utils/screenSize.js
@@ -2,15 +2,15 @@
 import isTouch from './isTouch'
 
 export default function screenSize() {
-    if (isTouch() && screen.width < 1025) {
-      return {
-        width: document.documentElement.clientWidth,
-        height: document.documentElement.clientHeight
-      }
-    } else {
-      return {
-        width: window.innerWidth,
-        height: window.innerHeight
-      }
+  if (isTouch() && screen.width < 1025) {
+    return {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight
     }
+  } else {
+    return {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
+  }
 }
